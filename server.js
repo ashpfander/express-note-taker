7e042cb8d7f51cd8c206243aa25a9b26a867c308
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2024-02-06 16:04:11 -0500

    added get routes for index.html and notes.html pages

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const path = require('path');
 
-// Sets a port for the server from express.js
-const PORT = process.env.PORT || 3001;
-
 // Initializes the use of express.js
 const app = express();
+
+// Sets a port for the server from express.js
+const PORT = process.env.PORT || 3001;
 
 // Route to be able to grab everything from public folder
 app.use(express.static('public'));
@@ -14,6 +14,16 @@ app.use(express.static('public'));
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// GET route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Catch all GET route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // Let's express.js listen to the port so it runs the server and generates page(s)
 app.listen(PORT, () =>
